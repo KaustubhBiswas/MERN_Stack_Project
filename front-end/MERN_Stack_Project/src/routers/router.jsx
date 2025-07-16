3afb78e2170488adf5ada30e282650c27b0e6424
Kaustubh <kaustubhbiswas001@gmail.com>
2025-07-16 12:43:54 +0530

    minor changes and kubernetes YAML manifests

--- front-end/MERN_Stack_Project/src/routers/router.jsx
+++ front-end/MERN_Stack_Project/src/routers/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import Cart from "../pages/Cart";
